d88d54f26b2eea8e41aea6712c1e6b0df5ad7302
vinhdiep <vtd1@txstate.edu>
2015-02-27 11:49:30 -0600

    Function f(t)=cos(omega*t)*sin(omega*t)*u(t-1) was plotted additionally to the file.

--- MatlabCode1.m
+++ MatlabCode1.m
@@ -1,7 +1,7 @@
-% EExxxx Project 1
-% MATLAB code for xxxxxxxxxxxxx
-% Your Name and Last Name
-% Date
+% EE4377Project 1
+% MATLAB code for function name
+% Vinh Diep
+% 2/26/2015
 
 clc      % Clear screen 
 clear    % Clear all variables
@@ -9,10 +9,14 @@ A = 3; f= 1000; omega = 2*pi*f;
 angle = degtorad(33); T=1/f;
 t=linspace(-3*T,3*T,1000);
 x_t=3*cos(omega*t+angle);
-subplot(3,1,1)
+subplot(4,1,1)
 plot(t,x_t, 'linewidth',2);grid on
-subplot(3,1,2)
+subplot(4,1,2)
 u_t=heaviside(t);
 plot(t,u_t, 'linewidth',2);grid on
-subplot(3,1,3)
+subplot(4,1,3)
 plot(t,x_t.*u_t, 'linewidth',2);grid on
+
+subplot(4,1,4);
+ft = cos(omega*t).*sin(omega*t).*heaviside(t-.001);
+plot(t, ft, 'linewidth', 2);grid on
